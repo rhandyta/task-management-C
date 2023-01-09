c635796f55a3e4f461aac4ea3bf66bcfe0a09bf0
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-09 22:11:41 +0700

    Setup firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+    apiKey: "AIzaSyDImjn2gMXP88tkNamQVYTvjwC3dZ9xg1U",
+    authDomain: "my-task-7df75.firebaseapp.com",
+    projectId: "my-task-7df75",
+    storageBucket: "my-task-7df75.appspot.com",
+    messagingSenderId: "276600237754",
+    appId: "1:276600237754:web:31a00801c4006f3ac11c83",
+    measurementId: "G-KFM099PF1E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
