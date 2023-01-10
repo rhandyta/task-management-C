5e0f00a3aa2bdb6be793865aee71589e9995212c
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-01-10 14:33:56 +0700

    setup redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+let persistor = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <PersistGate loading={<p>Loading...</p>} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
+);
